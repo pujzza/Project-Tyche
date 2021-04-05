bea09df25dd02cb83a80f4e57cdbad267966e405
pujzza <spujas3@gmail.com>
2021-04-05 23:54:29 +0530

    New Products

--- suraj-demo1/src/app/pages/Stock/ItemsManager/new-product/new-product.component.ts
+++ suraj-demo1/src/app/pages/Stock/ItemsManager/new-product/new-product.component.ts
@@ -1,15 +1,34 @@
+import { CommonService } from 'src/app/services/common.service';
 import { Component, OnInit } from '@angular/core';
+import { ProductsModel } from 'src/app/entities/StockModels';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.scss']
+  styleUrls: ['./new-product.component.scss'],
 })
 export class NewProductComponent implements OnInit {
-  sampleItem = ['item1','item2','item3']
-  constructor() { }
-
-  ngOnInit(): void {
+  warehouses = ['Chennai', 'Amritsar','Mumbai', 'Kolkata','Delhi', 'Banglore', 'Hyderabad'];
+  product: ProductsModel = new ProductsModel();
+  constructor(public service: CommonService) {
+    this.product.oauth = this.service.Oauth;
   }
 
+  ngOnInit(): void {}
+
+  AddProduct() {
+    this.service.AddProduct(this.product).subscribe(
+      (res) => {
+        if (res && res.returncode == 200) {
+          this.service.OpenSnackBar('Product Created!', 'SUCCESS');
+          this.product = new ProductsModel();
+        } else {
+          this.service.OpenSnackBar('Product not created!', 'FAILED');
+        }
+      },
+      (error) => {
+        this.service.OpenSnackBar('Something went wrong', 'SORRY');
+      }
+    );
+  }
 }
