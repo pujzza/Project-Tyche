fc6ed7c4b6c281b631386eea5b84118f4aa6fc40
pujzza <spujas3@gmail.com>
2021-04-05 23:54:13 +0530

    Manage products

--- suraj-demo1/src/app/pages/Stock/ItemsManager/manage-product/manage-product.component.ts
+++ suraj-demo1/src/app/pages/Stock/ItemsManager/manage-product/manage-product.component.ts
@@ -1,5 +1,7 @@
+import { CommonService } from 'src/app/services/common.service';
 import { Component, OnInit } from '@angular/core';
 import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
+import { ProductsModel } from 'src/app/entities/StockModels';
 
 @Component({
   selector: 'app-manage-product',
@@ -7,29 +9,33 @@ import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
   styleUrls: ['./manage-product.component.scss'],
 })
 export class ManageProductComponent implements OnInit {
-  rows = [
-    { sno: 1, prodName: 'A4 Paper', qty: 3000, code: 'P-A4', catg: 'Paper' ,price: 200 },
-    { sno: 2, prodName: 'A3 Paper', qty: 300, code: 'P-A3', catg: 'Paper' ,price: 90 },
-    { sno: 8, prodName: 'A4 Paper', qty: 3000, code: 'P-A4', catg: 'Paper' ,price: 9 },
-  ];
   loadingIndicator = true;
   reorderable = true;
-  data = [];
-
-  columns = [
-    { prop: 'sno', name: '#' },
-    { prop: 'prodName', name: 'Product Name' },
-    { prop: 'qty', name: 'Quantity' },
-    { prop: 'code', name: 'Code' },
-    { prop: 'catg', name: 'Category' },
-    { prop: 'price', name: 'Price' },
-    { name: 'Settings' },
-  ];
-  colWidth = (100 / this.columns.length)
-
+  data : ProductsModel[] = [];
   ColumnMode = ColumnMode;
   SortType = SortType;
-  constructor() {}
+  columns = [
+    { prop: 'id', name: '#',width: 10 },
+    { prop: 'ProductCode', name: 'Code',width: 50 },
+    { prop: 'ProductName', name: 'Product Name',width: 100 },
+    { prop: 'ProductCategory', name: 'Category',width: 100 },
+    { prop: 'StockUnit', name: 'Quantity',width: 50 },
+    { prop: 'ProductRetailPrice', name: 'Retail Price',width: 50 },
+    { prop: 'ProductWholesalePrice', name: 'Wholesale Price',width: 70 },
+    { name: 'Settings',width: 100 },
+  ];
+  constructor(public service: CommonService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    document.getElementById('ngxtable').style.height = `${screen.height - 170}px`;
+    this.getAllProducts();
+  }
+
+  getAllProducts(){
+    this.service.GetAllProducts().subscribe(res => {
+      if(res && res.returncode == 200){
+        this.data = res.returndata;
+      }
+    })
+  }
 }
