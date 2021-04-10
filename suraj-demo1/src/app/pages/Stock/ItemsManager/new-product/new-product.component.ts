ad18306fcaea4f0fce80be3b61d409b4741933f7
pujzza <spujas3@gmail.com>
2021-04-11 02:26:13 +0530

    New Product - Updates

--- suraj-demo1/src/app/pages/Stock/ItemsManager/new-product/new-product.component.ts
+++ suraj-demo1/src/app/pages/Stock/ItemsManager/new-product/new-product.component.ts
@@ -1,6 +1,6 @@
 import { CommonService } from 'src/app/services/common.service';
 import { Component, OnInit } from '@angular/core';
-import { ProductsModel } from 'src/app/entities/StockModels';
+import { InventoryItem, ProductsModel, SubProducts } from 'src/app/entities/StockModels';
 
 @Component({
   selector: 'app-new-product',
@@ -10,25 +10,52 @@ import { ProductsModel } from 'src/app/entities/StockModels';
 export class NewProductComponent implements OnInit {
   warehouses = ['Chennai', 'Amritsar','Mumbai', 'Kolkata','Delhi', 'Banglore', 'Hyderabad'];
   product: ProductsModel = new ProductsModel();
+  itemList : InventoryItem[] = [];
+  subProds : SubProducts[] = [];
+  saveError = '';
+  noprodcatgerror ='';
+
   constructor(public service: CommonService) {
     this.product.oauth = this.service.Oauth;
   }
 
-  ngOnInit(): void {}
-
-  AddProduct() {
-    this.service.AddProduct(this.product).subscribe(
-      (res) => {
-        if (res && res.returncode == 200) {
-          this.service.OpenSnackBar('Product Created!', 'SUCCESS');
-          this.product = new ProductsModel();
-        } else {
-          this.service.OpenSnackBar('Product not created!', 'FAILED');
+  ngOnInit(): void {
+    this.service.GetInventory().subscribe(
+      res => {
+        if(res && res.returncode == 200){
+          this.itemList = res.returndata;
         }
-      },
-      (error) => {
-        this.service.OpenSnackBar('Something went wrong', 'SORRY');
       }
     );
+  }
+
+  AddProduct() {
+    var unsaved = this.subProds.filter(x => x.isSaved==false).length;
+    if(unsaved && unsaved >0){
+      this.saveError = "Save all the above items"
+    } else {
+      this.saveError = ""   
+      this.product.oauth = this.service.Oauth;
+      this.product.products = this.subProds;
+      console.log(this.product)
+    }
+  }
+
+  AddSubProd(){
+    if(this.product.ProductCategory){
+      const prod = new SubProducts();
+      this.subProds.push(prod);  
+      this.noprodcatgerror=''
+    } else {
+      this.noprodcatgerror = "Required";
+    }
+  }
+
+  DeleteSubProd(index){
+    this.subProds.splice(index, 1);
+  }
+
+  saveSubProd(prod){
+    prod['isSaved'] = true;
   }
 }
