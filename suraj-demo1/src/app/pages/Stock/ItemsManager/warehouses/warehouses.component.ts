46afd124e183002f262d01974291ea86150c82e8
pujzza <spujas3@gmail.com>
2021-04-11 02:26:28 +0530

    Warehouse - updates

--- suraj-demo1/src/app/pages/Stock/ItemsManager/warehouses/warehouses.component.ts
+++ suraj-demo1/src/app/pages/Stock/ItemsManager/warehouses/warehouses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
+import { NewWarehouse } from 'src/app/entities/StockModels';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-warehouses',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarehousesComponent implements OnInit {
 
-  constructor() { }
+  loadingIndicator = true;
+  reorderable = true;
+  ColumnMode = ColumnMode;
+  SortType = SortType;
+  columns = [
+    { prop: 'id', name: '#',width: 10 },
+    { prop: 'itemname', name: 'Warehouse Name',width: 100 },
+    { prop: 'city', name: 'City',width: 70 },
+    { prop: 'contact', name: 'ContactNo',width: 50 },
+    { prop: 'totalproduct', name: 'Total Product',width: 50 },
+    { prop: 'totalstock', name: 'Total Stock',width: 50 },
+    { name: 'Settings',width: 100 },
+  ];
 
-  ngOnInit(): void {
+  isnewWarehouse = false;
+
+  warehouse = new NewWarehouse();
+  constructor(public service: CommonService) {
   }
 
+  ngOnInit(): void {
+    document.getElementById('ngxtable').style.height = `${screen.height - 170}px`;
+  }
+
+  closeNewWarehouse(){
+    this.isnewWarehouse = false;
+    this.warehouse = new NewWarehouse();
+  }
+
+  AddWarehouse(){
+    console.log(this.warehouse);
+    this.closeNewWarehouse();
+  }
 }
